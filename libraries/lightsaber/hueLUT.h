af85aa51375e8d563034717749030416ead85023
git-huber290 <minecraftgod977@gmail.com>
2025-06-15 11:19:13 -0400

    Update hueLUT.h
    Removed the base_hue LUT to save 32% of the program storage space.

--- libraries/lightsaber/hueLUT.h
+++ libraries/lightsaber/hueLUT.h
@@ -1,16 +1,14 @@
 #ifndef HUELUT_H
 #define HUELUT_H
 
-#include <Arduino.h>
+#include <Arduino.h> // For pgm_read_word
 #include <avr/pgmspace.h>
 
-#define CYCLE_LENGTH_MS 5000
 #define HALF_LEDS 108
 
-extern const uint16_t base_hue[CYCLE_LENGTH_MS + 1] PROGMEM;
 extern const uint16_t hue_offset[HALF_LEDS] PROGMEM;
 
-uint16_t getBaseHue(uint16_t m);
+// Function to get hue offset for a given LED number
 uint16_t getHueOffset(uint16_t led_num);
 
-#endif
+#endif // HUELUT_H
